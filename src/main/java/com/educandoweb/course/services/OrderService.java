ac51d2e061a3ba7e7ecd20a8f56e6419e7a88f3d
Thales Kirchner Ozéias <thales.ozeias@gmail.com>
2026-04-15 14:27:03 -0300

    Order entity, repository, service and resource

---
+++ src/main/java/com/educandoweb/course/services/OrderService.java
@@ -0,0 +1,24 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository; 
+	
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		return repository.findById(id).get();
+	}
+}
